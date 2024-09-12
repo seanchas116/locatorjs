00f15d815eb8bd00f2da285d20d79dba6951e90a
Ryohei Ikegami <iofg2100@gmail.com>
2024-09-12 19:55:38 +0900

    Locate linecol

--- packages/swc-jsx/src/lib.rs
+++ packages/swc-jsx/src/lib.rs
@@ -1,76 +1,97 @@
-use swc_common::FilePathMapping;
+use swc_common::sync::OnceCell;
 use swc_common::{sync::Lrc, SourceMap, DUMMY_SP};
+use swc_common::{LineCol, Spanned};
+use swc_core::common::SyntaxContext;
+use swc_core::ecma::ast::*;
 use swc_core::plugin::plugin_transform;
-use swc_core::plugin::proxies::TransformPluginProgramMetadata;
+use swc_core::plugin::proxies::{PluginSourceMapProxy, TransformPluginProgramMetadata};
 use swc_core::{ecma::ast::JSXElement, ecma::transforms::testing::test, ecma::visit::*};
-use swc_ecma_ast::*;
 use swc_ecma_parser::{Syntax, TsConfig};
-use swc_ecma_transforms_base::perf::Parallel;
-use swc_ecma_utils::quote_ident;
 use swc_ecma_visit::{as_folder, noop_visit_mut_type, Fold, VisitMut};
 
-/// `@babel/plugin-transform-react-jsx-source`
-pub fn jsx_src(dev: bool, cm: Lrc<SourceMap>) -> impl Fold + VisitMut {
-    as_folder(AddLocatorVisitor { cm, dev })
+struct LineColMapping {
+    line_cols: Vec<LineCol>,
 }
 
-impl Parallel for AddLocatorVisitor {
-    fn create(&self) -> Self {
-        self.clone()
+impl LineColMapping {
+    pub fn new(source: &str) -> Self {
+        let mut line_cols = Vec::new();
+        let mut line = 1;
+        let mut col = 0;
+        for c in source.chars() {
+            line_cols.push(LineCol {
+                line: line,
+                col: col,
+            });
+            if c == '\n' {
+                line += 1;
+                col = 0;
+            } else {
+                col += 1;
+            }
+        }
+        line_cols.push(LineCol {
+            line: line,
+            col: col,
+        });
+        LineColMapping {
+            line_cols: line_cols,
+        }
     }
-
-    fn merge(&mut self, _: Self) {}
 }
 
-#[derive(Clone)]
 struct AddLocatorVisitor {
-    cm: Lrc<SourceMap>,
-    dev: bool,
+    line_col_mapping: LineColMapping,
+    start_pos: u32,
+}
+
+impl AddLocatorVisitor {
+    pub fn new(line_col_mapping: LineColMapping) -> Self {
+        AddLocatorVisitor {
+            line_col_mapping,
+            start_pos: 0,
+        }
+    }
 }
 
 impl VisitMut for AddLocatorVisitor {
-    noop_visit_mut_type!();
+    fn visit_mut_program(&mut self, n: &mut Program) {
+        self.start_pos = n.span().lo.0;
+        n.visit_mut_children_with(self);
+    }
 
-    fn visit_mut_jsx_opening_element(&mut self, e: &mut JSXOpeningElement) {
-        if !self.dev || e.span == DUMMY_SP {
-            return;
-        }
+    fn visit_mut_jsx_element(&mut self, jsx: &mut JSXElement) {
+        let mut opening = jsx.opening.clone();
+        let start = opening.span.lo.0 - self.start_pos;
 
-        let loc = self.cm.lookup_char_pos(e.span.lo);
-        let file_name = loc.file.name.to_string();
+        let linecol = self.line_col_mapping.line_cols[start as usize];
+        println!("linecol: {:?}", linecol);
 
-        let locKey = format!("{}:{}:{}", file_name, loc.line, loc.col_display);
+        let loc = format!("{}:{}:{}", "file", linecol.line, linecol.col);
 
-        e.attrs.push(JSXAttrOrSpread::JSXAttr(JSXAttr {
-            span: e.span,
-            name: JSXAttrName::Ident(Ident::new("dataLocator".into(), e.span)),
-            value: Some(JSXAttrValue::Lit(Lit::JSXText(
-                JSXText {
-                    span: e.span,
-                    value: locKey.clone().into(),
-                    raw: locKey.clone().into(),
+        opening.attrs.push(JSXAttrOrSpread::JSXAttr(JSXAttr {
+            span: opening.span,
+            name: JSXAttrName::Ident(Ident::new("dataLocator".into(), opening.span)),
+            value: Some(JSXAttrValue::Lit(Lit::Str(
+                Str {
+                    span: opening.span,
+                    value: loc.clone().into(),
+                    raw: None,
                 }
                 .into(),
             ))),
         }));
-    }
-}
 
-fn tr() -> impl Fold {
-    let cm = Lrc::new(SourceMap::new(FilePathMapping::empty()));
-    jsx_src(true, cm)
+        // Assigning modified opening back to jsx object.
+        jsx.opening = opening;
+    }
 }
 
 #[plugin_transform]
 pub fn process(program: Program, metadata: TransformPluginProgramMetadata) -> Program {
-    let cm = Lrc::new(metadata.source_map);
-
-    // TODO continue here!!!!!
-
-    ---
-    
-    ;
-    program.fold_with(&mut jsx_src(true, cm))
+    let src = metadata.source_map.source_file.get().unwrap().src.clone();
+    let line_col_mapping = LineColMapping::new(&src);
+    program.fold_with(&mut as_folder(AddLocatorVisitor::new(line_col_mapping)))
 }
 
 test!(
@@ -78,8 +99,12 @@ test!(
         tsx: true,
         ..Default::default()
     }),
-    |_| tr(),
+    |_| {
+        as_folder(AddLocatorVisitor::new(LineColMapping::new(
+            "const x = <div />;",
+        )))
+    },
     test_one,
-    r#"const x = <div />;"#,
-    r#"const x = <div dataLocator="xx:1:5" />;"#
+    r#"const x = <div/>;"#,
+    r#"const x = <div dataLocator="file:1:10"/>;"#
 );
